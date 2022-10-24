3c5dd3e0dbbc6ffb134f6e3001f0ca9a30565419
osho210 <anysense.yamatsu@gmail.com>
2022-10-24 09:18:05 +0000

    [Add] ユーザー編集機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,24 @@ class UsersController < ApplicationController
   end
 
   def show
+    # 特定のuserの絞り込み
     @user = User.find(params[:id])
-    @post_images = @user.post_images 
+    @post_images = @user.post_images
   end
 
   def edit
+    # 特定のuserの絞り込み
+    @user = User.find(params[:id])
+  end
+  
+  def update
+	  @user = User.new(user_params)
+	  @user.save
+	  redirect_to user_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
