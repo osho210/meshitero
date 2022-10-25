f26a289eb4d58da2feb2262e6cfe560656861fcd
osho210 <anysense.yamatsu@gmail.com>
2022-10-25 12:46:56 +0000

    [Add] バリデーション・権限を設定

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,6 +4,9 @@ class PostImage < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  validates :shop_name, presence: true
+  validates :image ,presence:true
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
@@ -11,7 +14,7 @@ class PostImage < ApplicationRecord
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/png')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     image
   end
