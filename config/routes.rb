d11ffb6fc0baa5fbc0dc837875f0569c91692f60
osho210 <anysense.yamatsu@gmail.com>
2022-10-26 03:13:25 +0000

    [Add] Bootstrapの導入

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'homes/about' ,  to: 'homes#about' , as: 'about'
 
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show , :destroy]do
+  resources :post_images, only: [:new, :create, :index, :show , :destroy ]do
       resource :favorites, only: [:create, :destroy]
       resources :post_comments ,only: [:create , :destroy]
   end
